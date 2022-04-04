6fd3aac6588c222d4a793754f90cc613cff33b74
Henrique Melo <hacmelo@gmail.com>
2022-04-04 12:57:01 -0300

    Add: Global styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,37 @@
 <template>
-  <v-app>
+  <v-app class="app">
     <v-main>
       <Nuxt />
     </v-main>
+    <Footer />
   </v-app>
 </template>
 
 <script>
+import Footer from "./footer.vue";
+
 export default {
-  name: 'DefaultLayout',
-}
+  name: "DefaultLayout",
+  components: {
+    Footer,
+  },
+};
 </script>
+
+<style lang="scss">
+:root {
+  --primaryText: #fff;
+  --secondaryText: #f08d49;
+  --primaryBackground: #282c2c;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  color: var(--primaryText);
+}
+
+.app {
+  background-color: var(--primaryBackground) !important;
+}
+</style>
